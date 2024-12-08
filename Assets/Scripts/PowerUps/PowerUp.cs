feec9597c538fc47c7aeed441e0d6a502d25fd96
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-12-08 18:54:26 +0500

    08.12.24
    added price progression

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PowerUp : MonoBehaviour
 {
+    private const int Multiplier = 2;
+
     [SerializeField] private int _price;
     [SerializeField] protected string _name;
     [SerializeField] private string _description;
     [SerializeField] protected List<Toggle> _cages;
 
+    public event Action Upgraded;
+
     protected int _cagesFilledCount;
     public string Description => _description;
     public int Price => _price;
     public IReadOnlyList<Toggle> Cages => _cages;
+
+    private void Start() => 
+        ChangePrice();
 
     public void Upgrade()
     {
@@ -20,9 +28,11 @@ public class PowerUp : MonoBehaviour
         {
             if (cage.isOn == false)
             {
-                cage.isOn = true;
+                cage.isOn = true;                
 
                 _cagesFilledCount++;
+
+                Upgraded?.Invoke();
 
                 break;
             }
@@ -32,4 +42,10 @@ public class PowerUp : MonoBehaviour
     [ContextMenu("Refresh upgrades")]
     public void Refresh() =>
         _cagesFilledCount = 0;
+
+    private void ChangePrice()
+    {
+        for (int i = 0; i < _cagesFilledCount; i++)
+            _price *= Multiplier;
+    }
 }
--- Assets/Scripts/PowerUps/PowerUpView.cs
+++ Assets/Scripts/PowerUps/PowerUpView.cs
@@ -7,7 +7,16 @@ public class PowerUpView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _costView;
     [SerializeField] private TextMeshProUGUI _descriptionView;
 
-    private void Start()
+    private void Start() => 
+        Show();
+
+    private void OnEnable() => 
+        _powerUp.Upgraded += Show;
+
+    private void OnDisable() => 
+        _powerUp.Upgraded -= Show;
+
+    private void Show()
     {
         _descriptionView.text = _powerUp.Description;
         _costView.text = _powerUp.Price.ToString();
