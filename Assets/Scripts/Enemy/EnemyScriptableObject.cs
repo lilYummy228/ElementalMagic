4e68a0a7f6405b99cc3cdfc45d0ab59bb4e64ad8
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-12-23 03:15:12 +0500

    23.12.24
    fixed UI bugs

--- Assets/Scripts/Enemy/EnemyScriptableObject.cs
+++ Assets/Scripts/Enemy/EnemyScriptableObject.cs
@@ -1,4 +1,4 @@
-using System;
+using Lean.Localization;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +6,38 @@ using UnityEngine;
 public class EnemyScriptableObject : ScriptableObject
 {
     [SerializeField] private Transform _prefab;
-    [SerializeField] private string _name;
     [SerializeField] private int _healthValue;
     [SerializeField] private int _damageValue;
     [SerializeField] private int _attackDelay;
     [SerializeField] private int _award;
+    [Header("Localization")]
+    [SerializeField] private string _russianName;
+    [SerializeField] private string _englishName;
+    [SerializeField] private string _turkishName;
     [Header("Resistances")]
     [SerializeField] private int _fireResistance;
     [SerializeField] private int _waterResistance;
     [SerializeField] private int _earthResistance;
     [SerializeField] private int _windResistance;
 
+    public LeanLocalization Localization => FindObjectOfType<LeanLocalization>();
     public Transform Prefab => _prefab;
-    public string Name => _name;
+    public string Name
+    {
+        get
+        {
+            return Localization.CurrentLanguage switch
+            {
+                "Russian" => _russianName,
+                "English" => _englishName,
+                "Turkey" => _turkishName,
+                _ => _russianName,
+            };
+        }
+    }
     public int HealthValue => _healthValue;
     public int DamageValue => _damageValue;
     public int AttackDelay => _attackDelay;
     public int Award => _award;
-    public IReadOnlyList<int> Resistances => new List<int>() { _fireResistance, _waterResistance, _earthResistance, _windResistance};
+    public IReadOnlyList<int> Resistances => new List<int>() { _fireResistance, _waterResistance, _earthResistance, _windResistance };
 }
